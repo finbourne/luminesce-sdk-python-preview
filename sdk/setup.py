f036836ba7e7f11cc5f76756dfad035d1e410bea
Paul Saunders <paul.saunders@finbourne.com>
2022-08-15 13:43:00 +0100

    CTECH-1723: Unifies dependency versions with other sdks.
    Signed-off-by: Paul Saunders <paul.saunders@finbourne.com>

--- sdk/setup.py
+++ sdk/setup.py
@@ -32,11 +32,13 @@ from setuptools import setup, find_packages  # noqa: H301
 # http://pypi.python.org/pypi/setuptools
 
 REQUIRES = [
-    "python-dateutil >= 2.5.3",
-    "six >= 1.10",
-    "urllib3 >= 1.25.3",
-    "requests >= 2.21.0",
     "certifi >= 14.05.14",
+    "six >= 1.10",
+    "python-dateutil >= 2.5.3",
+    "setuputils >= 21.0.0"
+    "urllib3 >= 1.26.9",
+    "requests >= 2.27.1",
+    "pytz >= 2019.1"
     "finbourne-sdk-utilities"
 ]
 
